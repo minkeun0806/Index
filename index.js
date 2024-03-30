133ad8adfbc713ebd6f22d9a1eea6dbe54751577
minkeun0806 <66945732+minkeun0806@users.noreply.github.com>
2024-03-30 13:57:38 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -10,14 +10,4 @@ $(document).ready(function() {
 	$("#scrollBtn").on('click',function(){
 		$('body, html').animate({ scrollTop: Math.round( $("section[id='자기소개']").offset().top) }, 500);
 	});
-
-	$(window).scroll(function() {
-		if($(window).scrollTop() >= $("article:nth-of-type(1)").offset().top - 1 ){
-			$('header').fadeIn(1000);
-			$('video').hide();
-		} else {
-			$('header').fadeOut(1000);
-			$('video').show();
-		};
-	});
 });
