f54cd4373401d4c0d29c26a69398089057d06525
minkeun0806 <66945732+minkeun0806@users.noreply.github.com>
2024-04-23 17:55:37 +0900

    24.04.23. 수정3

--- index.js
+++ index.js
@@ -3,27 +3,31 @@ $(document).ready(function() {
 		var menuNo = $(this).attr("href").replace('#', '');;
 		var boxPosition = Math.round( $("section[id=" + menuNo + "]").offset().top);
 		$('body, html').animate({ scrollTop: boxPosition }, 500);
-		navReaction();
+		navReaction('close');
 	});
 	$('#logo').on('click',function(){
 		$('body, html').animate({ scrollTop: 0 }, 500);
-		navReaction();
+		navReaction('close');
 	});
 	$('#scrollBtn').on('click',function(){
 		$('body, html').animate({ scrollTop: Math.round( $("section[id='자기소개']").offset().top) }, 500);
-		navReaction();
+		navReaction('close');
 	});
 	$('#모바일메뉴').on('click',function(){
-		navReaction();
+		if($(this).hasClass('nav-actived')){
+			navReaction('open');
+		} else {
+			navReaction('close');
+		};
 	});
 });
 
-function navReaction(){
-	if($('#모바일메뉴').hasClass('nav-actived')){
-		$('#모바일메뉴').removeClass('nav-actived');
-		$('nav').removeClass('max-[640px]:fixed max-[640px]:top-[62px] max-[640px]:right-[0px]').addClass('max-[640px]:hidden');
-	} else {
+function navReaction(_action){
+	if(_action == 'open'){
 		$('#모바일메뉴').addClass('nav-actived');
 		$('nav').removeClass('max-[640px]:hidden').addClass('max-[640px]:fixed max-[640px]:top-[62px] max-[640px]:right-[0px]');
+	} else if(_action == 'close') {
+		$('#모바일메뉴').removeClass('nav-actived');
+		$('nav').removeClass('max-[640px]:fixed max-[640px]:top-[62px] max-[640px]:right-[0px]').addClass('max-[640px]:hidden');
 	};
 };
